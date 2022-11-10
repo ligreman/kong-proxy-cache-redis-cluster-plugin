dc869ea3bd662a790266282b760836e7dc3e7fbb
Ligreman <ligreman@users.noreply.github.com>
2022-11-10 21:18:24 +0100

    Update handler.lua

--- kong/plugins/proxy-cache-redis-cluster/handler.lua
+++ kong/plugins/proxy-cache-redis-cluster/handler.lua
@@ -223,7 +223,7 @@ local function store_cache_value(premature, conf, req_body, status, proxy_cache)
 end
 
 local ProxyCacheHandler = {
-    VERSION = "1.0.0-1",
+    VERSION = "1.0.0-2",
     PRIORITY = 902,
 }
 
